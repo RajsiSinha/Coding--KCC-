6594e4364c7138bb0d43dd077f1e3dd9e3cc2712
RajsiSinha <132333864+RajsiSinha@users.noreply.github.com>
2023-05-19 14:26:53 +0530

    Create Basic Data Types.cpp

---
+++ Assignment 0/Basic Data Types.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a;
+    long b;
+    char j;
+    float k;
+    double g;
+    cin>>a>>b>>j>>k>>g;
+    cout<<a<<"\n"<<b<<"\n"<<j<<"\n"<<setprecision(9)<<k<<"\n"<<g;
+    return 0;
+    }
+    
+
+    
